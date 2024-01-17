875d368d592b5a10ae670236f68ca6f96f83c78f
Nguyen Manh Hong <nmanhhong06@gmail.com>
2024-01-17 20:26:35 +0700

    fix

--- src/controllers/media.controller.ts
+++ src/controllers/media.controller.ts
@@ -1,9 +1,8 @@
 import { NextFunction, Request, Response } from 'express'
-import { mediaServices } from '~/services/media.service'
 
 export const uploadImagesController = async (req: Request, res: Response, next: NextFunction) => {
-  const url = await mediaServices.handleUploadFile(req)
+  // const url = await mediaServices.handleUploadFile(req)
   return res.json({
-    result: url
+    // result: url
   })
 }
